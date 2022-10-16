ea5d14f2d3870a983af0062ee95096abe50f8720
Mihir Mirchandani <mihirm@berkeley.edu>
2022-10-16 09:35:46 -0700

    almost there

--- writer.py
+++ writer.py
@@ -44,12 +44,93 @@ def addExpected(d):
 
     new_dict = {"Date" : str(date.today()), "name" : "expected", "values" : d}
     jsonObject.append(new_dict)
-    print(jsonObject)
     f = open("test.json", "w")
     json.dump(jsonObject, f)
     f.close()
 
     return _corsify_actual_response(jsonify({'yoyoyo my name is joe': 200}))
+
+
+# points system
+# if it is going down
+
+def calculate_points(tvd):
+    if tvd <= 0.1:
+        return 100
+    elif tvd <= 0.15:
+        return 75
+    elif tvd <= 0.2:
+        return 50
+    elif tvd <= 0.25:
+        return 25
+    elif tvd <= .5:
+        return 15
+    elif tvd <= .9:
+        return 5
+    else:
+        return 0
+
+@app.route('/findTotalScore/', methods=["POST"])
+def find_total_score():
+    _build_cors_preflight_response()
+    with open("test.json") as jsonFile:
+        jsonObject = json.load(jsonFile)
+        jsonFile.close()
+
+    total = 0
+    prev = 0
+    multiplier = 0
+    for i in range(len(jsonObject)):
+        if jsonObject[i]["name"] == "actual":
+            curr = jsonObject[i]["points"]
+            if curr >= prev:
+                total += (multiplier + 1) * curr
+                multiplier += 1
+            else:
+                total += curr
+                multipler = 0
+            prev = curr
+    return _corsify_actual_response(jsonify({'total': total}))
+
+
+def calculate_TVD():
+    with open("test.json") as jsonFile:
+        jsonObject = json.load(jsonFile)
+        jsonFile.close()
+    expected_values = (jsonObject[-2]["values"])
+    actual_values = (jsonObject[-1]["values"])
+    expected_distribution = []
+    actual_distribution = []
+    for entry in expected_values:
+        expected_distribution.append(expected_values[entry])
+    expected_sum = sum(expected_distribution)
+    for i in range(len(expected_distribution)):
+         expected_distribution[i] /= expected_sum
+
+
+    for entry in actual_values:
+        actual_distribution.append(actual_values[entry])
+    actual_sum = sum(actual_distribution)
+    for i in range(len(actual_distribution)):
+        actual_distribution[i] /= actual_sum
+
+    total = 0
+    for i in range(len(expected_distribution)):
+        total += abs(expected_distribution[i] - actual_distribution[i])
+    tvd_result = 0.5 * total
+
+    myObj = jsonObject[-1].copy()
+    myObj["TVD"] = tvd_result
+
+    jsonObject.pop()
+    jsonObject.append(myObj)
+
+    f = open("test.json", "w")
+    json.dump(jsonObject, f)
+    f.close()
+
+    return tvd_result
+
 
 @app.route("/addActual/<d>", methods=["POST"])
 def addActual(d):
@@ -77,7 +158,6 @@ def addActual(d):
         jsonObject.pop()
     new_dict = {"Date" : str(date.today()), "name" : "actual", "values" : d}
     jsonObject.append(new_dict)
-    print(jsonObject)
     f = open("test.json", "w")
     json.dump(jsonObject, f)
     f.close()
@@ -90,7 +170,6 @@ def addActual(d):
 
     myObj = jsonObject[-1].copy()
     myObj["points"] = points
-    print(myObj)
 
     jsonObject.pop()
     jsonObject.append(myObj)
@@ -101,8 +180,6 @@ def addActual(d):
 
     return _corsify_actual_response(jsonify({'yoyoyo my name is BILL': 200}))
 
-
-# flask this up
 @app.route('/readActual/', methods=["POST"])
 def read_actual():
     _build_cors_preflight_response()
@@ -113,46 +190,7 @@ def read_actual():
         return _corsify_actual_response(jsonify(jsonObject[-1]["values"]))
     return _corsify_actual_response(jsonify({}))
 
-def calculate_TVD():
-    with open("test.json") as jsonFile:
-        jsonObject = json.load(jsonFile)
-        jsonFile.close()
-    expected_values = (jsonObject[-2]["values"])
-    actual_values = (jsonObject[-1]["values"])
-    expected_distribution = []
-    actual_distribution = []
-    for entry in expected_values:
-        expected_distribution.append(expected_values[entry])
-    expected_sum = sum(expected_distribution)
-    for i in range(len(expected_distribution)):
-         expected_distribution[i] /= expected_sum
 
-
-    for entry in actual_values:
-        actual_distribution.append(actual_values[entry])
-    actual_sum = sum(actual_distribution)
-    for i in range(len(actual_distribution)):
-        actual_distribution[i] /= actual_sum
-    print("Expected: " + str(expected_distribution))
-    print("Actual: " + str(actual_distribution))
-
-    total = 0
-    for i in range(len(expected_distribution)):
-        total += abs(expected_distribution[i] - actual_distribution[i])
-    tvd_result = 0.5 * total
-
-    myObj = jsonObject[-1].copy()
-    myObj["TVD"] = tvd_result
-    print(myObj)
-
-    jsonObject.pop()
-    jsonObject.append(myObj)
-
-    f = open("test.json", "w")
-    json.dump(jsonObject, f)
-    f.close()
-
-    return tvd_result
 
 @app.route('/readTVD/', methods=["POST"])
 def read_TVD():
@@ -160,30 +198,14 @@ def read_TVD():
     with open("test.json") as jsonFile:
         jsonObject = json.load(jsonFile)
         jsonFile.close()
-    if jsonObject[-1]["name"] == "expected":
-        # return -1
+
+
+    if len(jsonObject) == 0:
         return _corsify_actual_response(jsonify({'TVD': -1.0}))
-    # return jsonObject[-1]["TVD"]
-    return _corsify_actual_response(jsonify({'TVD': jsonObject[-1]["TVD"]}))
-
-# points system
-# if it is going down
-
-def calculate_points(tvd):
-    if tvd <= 0.1:
-        return 100
-    elif tvd <= 0.15:
-        return 75
-    elif tvd <= 0.2:
-        return 50
-    elif tvd <= 0.25:
-        return 25
-    elif tvd <= .5:
-        return 15
-    elif tvd <= .9:
-        return 5
+    elif jsonObject[-1]["name"] == "expected":
+        return _corsify_actual_response(jsonify({'TVD': -1.0}))
     else:
-        return 0
+        return _corsify_actual_response(jsonify({'TVD': jsonObject[-1]["TVD"]}))
 
 
 def _build_cors_preflight_response():
